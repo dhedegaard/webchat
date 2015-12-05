279982985a9b1720f24316cce7e4988026a42e97
Dennis Hedegaard <dennis@dhedegaard.dk>
2015-12-05 18:11:35 +0100

    urls are now referenced with the view methods instead of strings, fix for deprecation warning in 1.9

--- webchat/urls.py
+++ webchat/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
+from . import views
+
 urlpatterns = patterns('',
-    url(r'^$', 'webchat.views.index', name='index'),
-    url(r'^send$', 'webchat.views.send', name='send'),
-    url(r'^get_new$', 'webchat.views.get_new', name='get_new'),
+    url(r'^$', views.index, name='index'),
+    url(r'^send$', views.send, name='send'),
+    url(r'^get_new$', views.get_new, name='get_new'),
 )
