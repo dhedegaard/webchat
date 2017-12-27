752cee11509ff98d3a9a43e6af9ef4603559e575
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-12-27 11:40:32 +0100

    Fixed old import no longer valid after 2.0, in test

--- webchat/tests.py
+++ webchat/tests.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 import mock
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test import TestCase
 
 from .models import Message
