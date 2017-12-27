d546240d18a02826218e49680c52ec5f36f241b5
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-12-27 11:38:47 +0100

    webpack.config: Converted ' to "

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,49 +1,49 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: [
-        'babel-polyfill',  // For Promise on IE.
-        'whatwg-fetch',  // For fetch on IE.
-        './index.tsx',
-        './scss/style.scss'
+        "babel-polyfill",  // For Promise on IE.
+        "whatwg-fetch",  // For fetch on IE.
+        "./index.tsx",
+        "./scss/style.scss"
     ],
     output: {
-        path: path.resolve('../webchat/static'),
-        filename: 'bundle.js'
+        path: path.resolve("../webchat/static"),
+        filename: "bundle.js"
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                use: 'babel-loader',
+                use: "babel-loader",
                 exclude: /node_modules/
             },
             {
                 test: /\.tsx?$/,
-                use: ['babel-loader', 'ts-loader'],
+                use: ["babel-loader", "ts-loader"],
                 exclude: /node_modules/
             },
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
+                    fallback: "style-loader",
                     use: [{
-                        loader: 'css-loader',
+                        loader: "css-loader",
                         options: {
                             sourceMap: true
                         }
                     }, {
-                        loader: 'sass-loader',
+                        loader: "sass-loader",
                         options: {
                             sourceMap: true
                         }
                     }],
-                    publicPath: '../webchat/static'
+                    publicPath: "../webchat/static"
                 }),
                 exclude: /node_modules/
             }
@@ -51,7 +51,7 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: 'bundle.css',
+            filename: "bundle.css",
             disable: false,
             allChunks: true
         })
